7f2ddb0596495914e1a853e490d01f3c5717d298
NIYAZ04 <niyazhaque098@gmail.com>
2024-12-08 01:21:05 +0530

    prefinal polished

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ if (started) {
 }
 
 const createWindow = () => {
-  // Create the browser window.
+  
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -18,17 +18,16 @@ const createWindow = () => {
       disableHardwareAcceleration: true
     },
   });
-  //
+  //bro seriously 
 mainWindow.setAlwaysOnTop(true,'screen')
 
-  // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
 
-//  mainWindow.webContents.openDevTools();
+ mainWindow.webContents.openDevTools();
 };
 
 app.whenReady().then(() => {
@@ -45,6 +44,9 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+
+// in case you are reding this : You are doing great work in your life ...CC
+// Mir Niyazul Haque 
   }
 });
 
--- src/renderer.js
+++ src/renderer.js
@@ -1,103 +1,3 @@
-// import './index.css';
-// const backButton=document.getElementById("back-button");
-// const forwardButton=document.getElementById("forward-button");
-// const reloadButton=document.getElementById("reload-button");
-// const searchButton=document.getElementById("search-button");
-// const newWindowButton=document.getElementById("new-window-button");
-// const goButton=document.getElementById("go");
-// const tabsContainer = document.getElementById("tabs-container");
-// //url input 
-
-// const urlInputField=document.getElementById("url-input");
-// //weview 
-// const webview=document.getElementById("webview");
-// let url="";
-// urlInputField.addEventListener("keydown",(event)=>{
-//     if(event.key==="Enter"){
-//         event.preventDefault();
-//         handleUrl()
-//     }
-// })
-// goButton.addEventListener("click",(event)=>{
-//     event.preventDefault();
-//     handleUrl();
-// })
-
-// searchButton.addEventListener("click",()=>{
-// url="https://www.google.com"
-// urlInputField.value=url;
-// webview.src=url;
-// })
-
-// backButton.addEventListener("click",()=>{
-//     webview.goBack();
-// })
-// forwardButton.addEventListener("click",()=>{
-//     webview.goForward();
-
-// })
-
-// reloadButton.addEventListener("click",()=>{
-//     webview.reload();
-// })
-
-
-// webview.addEventListener("did-navigate",(event)=>{
-//     url=event.url;
-//     urlInputField.value=url;
-// })
-
-// newWindowButton.addEventListener("click",()=>{
-//     api.newWindow();
-    
-// })
-// function handleUrl(){
-//     let url="";
-//     const inputUrl=urlInputField.value;
-//    if(inputUrl.startsWith("http://")|| inputUrl.startsWith("https://")){
-//     url=inputUrl;
-//    }else {
-//     url = "http://"+inputUrl;
-
-//    }
-//     webview.src=url;
-
-// }
-
-
-
-// // Render Tabs
-
-
-// let tabs = [];
-// let currentTabIndex = 0;
-// function renderTabs() {
-//     tabsContainer.innerHTML = "";
-//     tabs.forEach((tab, index) => {
-//       const tabElement = document.createElement("div");
-//       tabElement.textContent = tab.title;
-//       tabElement.className = `tab ${index === currentTabIndex ? "active" : ""}`;
-//       tabElement.addEventListener("click", () => switchToTab(index));
-//       tabsContainer.appendChild(tabElement);
-//     });
-//   }
-  
-//   // Switch Tabs
-//   function switchToTab(index) {
-//     currentTabIndex = index;
-//     const tab = tabs[index];
-//     urlInputField.value = tab.url;
-//     webview.src = tab.url;
-//     renderTabs();
-//   }
-  
-//   // Initialize with the first tab
-//   tabs.push({ title: "Tab 1", url: "about:blank" });
-//   renderTabs();
-  
-
-
-
 import './index.css';
 
 const backButton = document.getElementById("back-button");
@@ -110,6 +10,16 @@ const goButton = document.getElementById("go");
 const urlInputField = document.getElementById("url-input");
 const webview = document.getElementById("webview");
 const tabsContainer = document.getElementById("tabs-container");
+//Color piacker 
+const colorPicker = document.getElementById("color-picker");
+
+colorPicker.addEventListener("input", (event) => {
+  const selectedColor = event.target.value;
+  document.body.style.backgroundColor = selectedColor;
+
+  document.querySelector("#browser-tools").style.backgroundColor = selectedColor;
+  document.querySelector("#tabs-container").style.backgroundColor = selectedColor;
+});
 
 let tabs = [];
 let currentTabIndex = 0;
@@ -202,3 +112,4 @@ function switchToTab(index) {
 // Initialize with the first tab
 tabs.push({ title: "Tab 1", url: "about:blank" });
 renderTabs();
+
